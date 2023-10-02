e0c03c954f73f872fe395e5c2760b463b90d534b
Nicolas Gallardo <nicolasgallardo1507@gmail.com>
2023-10-02 19:01:53 -0300

    BootstrapAgregado

--- AppNico/views.py
+++ AppNico/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def inicio(request):
     return render(request,"AppNico/index.html")
+
+def curso(request):
+    return HttpResponse("Bienvenido al curso de python")
+
+def estudiantes(request):
+    return HttpResponse("Hola Estudiantes!")
+
+def profesores(request):
+    return("Hola Profesores!")
 
 
