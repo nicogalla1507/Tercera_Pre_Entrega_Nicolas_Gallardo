e6c1599f166649ed3f04523aad815cf761bb92d7
Nicolas Gallardo <nicolasgallardo1507@gmail.com>
2023-10-04 00:17:16 -0300

    AdminAgregado

--- AppNico/admin.py
+++ AppNico/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-
+from .models import*
 # Register your models here.
 
+admin .site.register(Curso)
+admin.site.register(Estudiante)
+admin.site.register(Register)
+admin.site.register(Login)
